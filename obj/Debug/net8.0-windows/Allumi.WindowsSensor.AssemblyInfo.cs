33ee27943cc0190d7bd91443b2290d6f7fc1d702
fini-commits <fini@vetra.life>
2025-10-14 23:36:13 +0200

    chore: remove in-app Squirrel reference (handled in CI)

--- obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0e98f8fce566610f95421bb5f4f8223507e7ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12aad8664f571710d91c991269cb5987342cc1ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

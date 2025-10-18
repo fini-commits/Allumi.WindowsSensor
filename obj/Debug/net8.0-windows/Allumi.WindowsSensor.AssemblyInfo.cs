55509ba4a48098f12e436d054bdac3379438139b
fini-commits <fini@vetra.life>
2025-10-18 17:38:17 +0200

    Changes .NET 8 integration

--- obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03acde3194b7b9def4e8df597467e52573c31d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebe8918b5bbf85193d9c66d6f5b5afc64f7837e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

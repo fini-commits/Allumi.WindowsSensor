4c6a62e8f68904409611088bd8c30b1a1d95f065
fini-commits <fini@vetra.life>
2025-10-21 01:27:15 +0200

    FixAuth, hopefuly

--- obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7153989c9ed5e8d5173a7ce00d95dcf5e7d2aeac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03614062f540cb85856d4828cc48e89326df8bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

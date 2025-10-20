c2bb735bc29e14c69df1d3b937e489d4e5aead4f
fini-commits <fini@vetra.life>
2025-10-21 00:17:34 +0200

    Token Update

--- obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55509ba4a48098f12e436d054bdac3379438139b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8675961a8c32ef0df25632359eae4547bacefaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

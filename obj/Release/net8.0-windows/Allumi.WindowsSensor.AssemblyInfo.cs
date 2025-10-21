c516b4075b414720b7de5e3c15f85c9562321348
fini-commits <fini@vetra.life>
2025-10-21 22:41:27 +0200

    Fixed again the seession tracking

--- obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.53.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.53+65240ae5ceba4d3344bbac02f1b6456b1081f4e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.53+474f99908adb9c3e367c8098a7d6b0e56f745890")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.53.0")]

e864d3bc657755245b2dd05e83bf7e0211c721a1
fini-commits <fini@vetra.life>
2025-10-21 20:03:41 +0200

    Second try to fix Math Bold Text coming from YouTube.

--- obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a427ba6818f5d5d5eec719871285bac75f2277c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918202b89e0d11ebe0c69a1935558d68e2ecd4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

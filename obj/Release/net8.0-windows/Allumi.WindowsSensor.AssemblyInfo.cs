796707ac6756e7ef8f89184a81a9e9c9a41a6728
fini-commits <fini@vetra.life>
2025-10-21 04:56:30 +0200

    Fix Bug Stem

--- obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Allumi.WindowsSensor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecdee6f83fa10a54b8adb9e02c3df844513ddef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f5b9530659f6a2f5521935785d8a0fb7c81d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Allumi.WindowsSensor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
